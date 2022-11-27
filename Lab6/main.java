777b2326f5e71cd7910c326dadb0a77ddbd31c35
Alan Lai <37102053+Alsctiho@users.noreply.github.com>
2022-11-27 20:12:26 +0800

    Some update

---
+++ Lab6/main.java
@@ -0,0 +1,28 @@
+package Lab6;
+
+/*
+ * note the bad coding style (deliberately written for debugging exercise) 
+ */
+public class main {
+	public static void main(String[] args) {
+		Animal animals[] = new Animal[10];
+		// Bug here: Animal a is just a pointer to the array element,
+		// assign an object to a pointer doesnt modify the content in array.
+		//for (Animal a : animals)  	
+		//	a = new Animal();	
+
+		// Solution:
+		for(int i = 0; i < 10; i++)
+			animals[i] = new Animal();
+
+		for (int iii = 0; iii < 10; iii++) {
+			int ii = 0;
+			for (; ii < 100 && animals[iii].isAlive() ; ii++) {
+				System.out.print(animals[iii].getWeight() + " ");
+				animals[iii].eat();
+				if (ii % 3 == 0)
+				animals[iii].poo();
+			}
+		}	
+	}
+}
